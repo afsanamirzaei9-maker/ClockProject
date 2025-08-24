977e5025e4bc12efc3415f812297315e8d5972da
afsanamirzaei9-maker <afsanamirzaei9@gmail.com>
2025-08-24 21:04:51 +0430

    Update clock.js

--- clock.js
+++ clock.js
@@ -20,23 +20,14 @@ const timeStatus = (H > 12) ? 'PM' : 'AM';
   
 // there we set the time
   if (format === '12') {
-   
+   H =(H > 12) ? H % 12 : H;
   }
 
   clockNum.innerHTML = `${H} :${M} :${S}`;
   let time = document.querySelector('.time');
   time.innerHTML =` ${timeStatus}`;
 
-  //now set the AM and PM buttons
-let am = document.querySelector('.setPm #AM');
-am.addEventListener(`click`,()=>{
-  document.querySelector('.time').textContent = 'AM';
-})
 
-let pm = document.querySelector('.setPm #PM');
-pm.addEventListener(`click`,()=>{
-  document.querySelector('.time').textContent = 'PM';
-});
 
 }
  //now set the time format buttons
@@ -45,13 +36,14 @@ pm.addEventListener(`click`,()=>{
   button.addEventListener('click', ()=>{
     format = button.getAttribute('data-format');
     clockNum.setAttribute('data-format',format);
-    
+   MyClock();
   });
-  MyClock();
+  
  });
 
 // now we make our clock to see the second changes
 setInterval(MyClock, 1000);
+
 
 
 
